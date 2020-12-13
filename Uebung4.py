1c3acfb0c9a279bbed16a582e65997e2119a9cbf
Tanjiroo <71944973+Tanjiroo@users.noreply.github.com>
2020-12-13 22:59:35 +0100

    Spieler wurden angelegt

--- Uebung4.py
+++ Uebung4.py
@@ -8,4 +8,31 @@ def wuefeln():
     return x
 
 
-print(wuefeln())
+
+
+class Person:
+    def __init__(self, name):
+        self.name = name
+    
+
+
+
+#print(wuefeln())
+
+
+
+
+anzahl_der_spieler = int(input("Please enter a number of players: "))
+player = []
+for i in range(anzahl_der_spieler):
+    print("Please enter the name of", i, ". player")
+    player_name = input("Player's name: ")
+    i = Person(player_name)
+    player.append(i)
+
+
+
+
+
+
+    
