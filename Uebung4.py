e7bfba70de51cf07407528bd8614c57b49804359
bassam441 <bassamalmuhammad@gmail.com>
2020-12-18 15:38:39 +0100

    neu°_°

--- Uebung4.py
+++ Uebung4.py
@@ -1,13 +1,12 @@
 __author__ = "7396443, Almuhammad"
 
-import os
 import random
 import time
+
 print("==========\n"
       "16-ist-tot\n"
       "==========")
 
-time.sleep(0.5)
 
 print ("Ziel des Spiels:\n"
 "Es wird reihum gewürfelt.\n"
@@ -20,84 +19,66 @@ print ("Ziel des Spiels:\n"
 
 print("Regeln des Spiels:\n")
 
-time.sleep(1.0)
 
 print("-Wer 16 oder mehr erreicht, hat sofort verloren\n"
       "-Wer als Summenpunkte 9 erreicht, darf nicht mehr würfeln\n"
       "-Wer als Summenpunkte 10 erreicht, muss noch einmal werfen\n")
 
-time.sleep(1.0)
-
-
-def wuefeln():
-    """rtuzutzizu"""
-    x = random.randint(1,6)
-    return x
-
-
-
-def neustart():
-    neustart = input("Willst du noch eine Runde spielen? Ja[Y], Nein[N]\n")
-    
-    if neustart == "y":
-        main()
-    else:
-        neustart()
-
-
-
-
-class Person:
-    """rkjrhtre"""
-    def __init__(self, name):
-        self.name = name
-        self.punkte = 0
-
-    def sum(self, punkt):
-        self.punkte += punkt
-    
-"""
-def clear(): 
-    if name == 'nt': 
-        system('cls') 
-    else: 
-        system('clear') 
-
-
-"""
-
-anzahl_der_spieler = int(input("Please enter a number of players: "))
-player = []
-for i in range(1, anzahl_der_spieler+1):
-    print("Please enter the name of", i, ". player")
-    player_name = input("Player's name: ")
-    i = Person(player_name)
-    player.append(i)
-
-
-
-
-
-
-
-
-
-
 def main():
     """zrtzurzizu"""
+
+    def wuerfeln():
+        """rtuzutzizu"""
+        x = random.randint(1,6)
+        return x
+
+
+
+
+
+    class Person:
+        """rkjrhtre"""
+        def __init__(self, name):
+            self.name = name
+            self.punkte = 0
+
+        def sum(self, punkt):
+            self.punkte += punkt
+        
+        
+
+
+    anzahl_der_spieler = int(input("Please enter a number of players: "))
+    player = []
+    for i in range(1, anzahl_der_spieler+1):
+        print("Please enter the name of", i, ". player")
+        player_name = input("Player's name: ")
+        i = Person(player_name)
+        player.append(i)
+
+
+
+
+
+
+
+
+
+
+
     rounde = 1
     while len(player) != 1:
         if rounde == len(player):
             rounde = 0
         print("Spieler ", player[rounde-1].name, "am Zug")
         
-        wuerfel = wuefeln()
+        wuerfel = wuerfeln()
         player[rounde-1].sum(wuerfel)
         print(wuerfel)
         while True:
-            frage = input("Wollen Sie Nochmal Würfeln: ")
+            frage = input("Wollen Sie Nochmal Würfeln? Ja[y], Nein[n]\n ")
             if frage == "y":
-                wuerfel = wuefeln()
+                wuerfel = wuerfeln()
                 player[rounde-1].sum(wuerfel)
                 print(wuerfel)
                 print("Summe der Punkte", player[rounde-1].punkte)
@@ -107,7 +88,7 @@ def main():
                 if player[rounde-1].punkte == 10:
                     print("Du hast eine 10 dann du musst nochmal würfeln")
                     time.sleep(3.0)
-                    wuerfel = wuefeln()
+                    wuerfel = wuerfeln()
                     player[rounde-1].sum(wuerfel)
                     print(wuerfel)
                     print("Summe der Punkte", player[rounde-1].punkte)
@@ -133,19 +114,33 @@ def main():
             print("Gratuliere ", player[rounde-1].name, "Du hast gewonnen.")
                 
 
-        #player[rounde-1].punkte = 0
+        
             
         rounde += 1
-
-        
+    def Neustart():
+            time.sleep(0.5)
+            Neustart = input("Willst du noch eine Runde spielen? Ja[Y], Nein[N]\n")
+            
+            if Neustart.upper() == "Y":
+                    time.sleep(0.1)
+                    print("Viel Spaß!\n")
+                    time.sleep(0.1)
+                    main()
+            elif Neustart.upper() == "N":
+                    time.sleep(0.1)
+                    print("auf Wiedersehen !")
+                    quit()
+            else:
+                    time.sleep(0.1)
+                    Neustart()
+            
+    Neustart()
 if __name__ == "__main__":
    main()
 
 
     
-
         
-
 
 
         
